a32e08d00eda3ebd930c7a765a42a32caab628f7
teriTheDesigner <tere1109@stud.kea.dk>
2025-10-26 19:21:20 +0100

    Create signup form

---
+++ components/signup-form.tsx
@@ -0,0 +1,83 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+export function SignupForm({ onSubmit, loading, className, ...props }: any) {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSubmit(fullName, email, password);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={cn("flex flex-col gap-2", className)}
+      {...props}
+    >
+      <FieldGroup>
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h1 className="text-2xl font-bold">Create your account</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Fill in the form below to create your account
+          </p>
+        </div>
+        <Field>
+          <FieldLabel htmlFor="name">Full Name</FieldLabel>
+          <Input
+            id="name"
+            type="text"
+            placeholder="John Doe"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Input
+            id="email"
+            type="email"
+            placeholder="m@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="password">Password</FieldLabel>
+          <Input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <FieldDescription>
+            Must be at least 8 characters long.
+          </FieldDescription>
+        </Field>
+        <Field>
+          <Button type="submit" disabled={loading}>
+            {loading ? "Signing up..." : "Create Account"}
+          </Button>
+        </Field>
+
+        <Field>
+          <FieldDescription className="px-6 text-center">
+            Already have an account? <a href="/login">Log in</a>
+          </FieldDescription>
+        </Field>
+      </FieldGroup>
+    </form>
+  );
+}

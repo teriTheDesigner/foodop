c292a843901fcee57d6c5ff2322eb9ac89759ca9
teriTheDesigner <tere1109@stud.kea.dk>
2025-10-27 20:22:34 +0100

    Show correct items in the main navigation

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,58 +1,110 @@
-"use client"
+"use client";
 
-import { IconCirclePlusFilled, IconMail, type Icon } from "@tabler/icons-react"
+import {
+  IconChartBar,
+  IconCirclePlusFilled,
+  IconDashboard,
+  IconFolder,
+  IconListDetails,
+  IconMail,
+  IconUsers,
+  type Icon,
+} from "@tabler/icons-react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   SidebarGroup,
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
-export function NavMain({
-  items,
-}: {
-  items: {
-    title: string
-    url: string
-    icon?: Icon
-  }[]
-}) {
+export function NavMain() {
+  const pathname = usePathname();
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
-            <Button
-              size="icon"
-              className="size-8 group-data-[collapsible=icon]:opacity-0"
-              variant="outline"
-            >
-              <IconMail />
-              <span className="sr-only">Inbox</span>
-            </Button>
-          </SidebarMenuItem>
-        </SidebarMenu>
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
+          <SidebarMenuItem>
+            <Link href="/">
+              <SidebarMenuButton
+                tooltip="Dashboard"
+                className={`${
+                  pathname === "/"
+                    ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                    : ""
+                }`}
+              >
+                <IconDashboard />
+                <span>Dashboard</span>
               </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
+            </Link>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <Link href="/users">
+              <SidebarMenuButton
+                tooltip="Users"
+                className={`${
+                  pathname === "/users"
+                    ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                    : ""
+                } `}
+              >
+                <IconListDetails />
+                <span>Users</span>
+              </SidebarMenuButton>
+            </Link>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <Link href="/analytics">
+              <SidebarMenuButton
+                tooltip="Analytics"
+                className={`${
+                  pathname === "/analytics"
+                    ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                    : ""
+                }`}
+              >
+                <IconChartBar />
+                <span>Analytics</span>
+              </SidebarMenuButton>
+            </Link>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <Link href="/issues">
+              <SidebarMenuButton
+                tooltip="Issues"
+                className={`${
+                  pathname === "/issues"
+                    ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                    : ""
+                }`}
+              >
+                <IconFolder />
+                <span>Issues</span>
+              </SidebarMenuButton>
+            </Link>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <Link href="/team">
+              <SidebarMenuButton
+                tooltip="Team"
+                className={`${
+                  pathname === "/team"
+                    ? "bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+                    : ""
+                }`}
+              >
+                <IconUsers />
+                <span>Team</span>
+              </SidebarMenuButton>
+            </Link>
+          </SidebarMenuItem>
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
-  )
+  );
 }

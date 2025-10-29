c268677bfe9557e55bfa3dd2576f4063b8d1bbd8
teriTheDesigner <tere1109@stud.kea.dk>
2025-10-29 21:36:21 +0100

    Use user data from state

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { RootState } from "@/app/store/store";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import {
@@ -8,12 +9,10 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { useSelector } from "react-redux";
 
-export function NavUser({
-  user,
-}: {
-  user: { full_name: string; email: string };
-}) {
+export function NavUser() {
+  const user = useSelector((state: RootState) => state.user.user);
   const { isMobile } = useSidebar();
   return (
     <SidebarMenu>
@@ -23,11 +22,11 @@ export function NavUser({
           className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
         >
           <Avatar className="h-6 w-6 rounded-lg grayscale">
-            <AvatarImage src="/avatar.svg" alt={user.full_name} />
+            <AvatarImage src="/avatar.svg" alt="Username" />
             <AvatarFallback className="rounded-lg">CN</AvatarFallback>
           </Avatar>
           <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate font-medium">{user.full_name}</span>
+            <span className="truncate font-medium">Username</span>
             <span className="text-muted-foreground truncate text-xs">
               {user.email}
             </span>

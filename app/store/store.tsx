57d144b6965c62ac1b06a883e578ca7539ad0660
teriTheDesigner <tere1109@stud.kea.dk>
2025-10-29 21:32:23 +0100

    Create user store

---
+++ app/store/store.tsx
@@ -0,0 +1,35 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+  user: any | {};
+}
+
+const initialState: UserState = {
+  user: {},
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<any>) {
+      state.user = action.payload;
+    },
+    clearUser(state) {
+      state.user = {};
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    user: userSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;

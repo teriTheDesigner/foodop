113390c7cdbb4d83df920dc152503d9a7a8823fa
teriTheDesigner <tere1109@stud.kea.dk>
2025-11-09 17:41:01 +0100

    Don't set admin data on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,32 +2,21 @@
 import { useEffect } from "react";
 import { supabase } from "../lib/supabase";
 import { useRouter } from "next/navigation";
-import { AppSidebar } from "@/components/app-sidebar";
 import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-
 import data from "../data.json";
-import { useDispatch } from "react-redux";
-import { setAdmin } from "@/app/store/store";
 
 export default function Dashboard() {
   const router = useRouter();
-  const dispatch = useDispatch();
 
   useEffect(() => {
     const checkSession = async () => {
       const { data } = await supabase.auth.getSession();
-      if (!data.session) {
-        router.push("/login");
-      } else {
-        dispatch(setAdmin(data.session.user));
-      }
+      if (!data.session) router.replace("/login");
     };
     checkSession();
-  }, [router, dispatch]);
+  }, [router]);
 
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">

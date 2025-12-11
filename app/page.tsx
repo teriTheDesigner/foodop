acc769322051941c4942c0ab11b66fdf4e57c438
teriTheDesigner <tere1109@stud.kea.dk>
2025-12-11 16:22:17 +0100

    Check session on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
 "use client";
 
-export default function Home() {}
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { supabase } from "./lib/supabase";
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const { data } = await supabase.auth.getSession();
+      if (!data.session) router.replace("/login");
+    };
+    checkSession();
+  }, [router]);
+}

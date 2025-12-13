06dd41acedea354f685aa814e6f0e8a500aacd71
teriTheDesigner <tere1109@stud.kea.dk>
2025-12-13 15:59:00 +0100

    Redirect users on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   useEffect(() => {
     const checkSession = async () => {
       const { data } = await supabase.auth.getSession();
-      if (!data.session) router.replace("/login");
+      router.replace(data.session ? "/dashboard" : "/login");
     };
     checkSession();
   }, [router]);

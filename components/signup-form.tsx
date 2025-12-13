41074a700fd10d87449351d9f5823893515bc77e
teriTheDesigner <tere1109@stud.kea.dk>
2025-12-13 14:03:01 +0100

    Add placeholders

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,11 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field";
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
@@ -13,23 +8,18 @@ export function SignupForm({ onSubmit, loading, className, ...props }: any) {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    onSubmit(fullName, email, password);
+    onSubmit(fullName, email, password, role);
   };
   return (
-    <form
-      onSubmit={handleSubmit}
-      className={cn("flex flex-col gap-2", className)}
-      {...props}
-    >
+    <form onSubmit={handleSubmit} className={cn("flex flex-col gap-2", className)} {...props}>
       <FieldGroup>
         <div className="flex flex-col items-center gap-1 text-center">
           <h1 className="text-2xl font-bold">Create your account</h1>
-          <p className="text-muted-foreground text-sm text-balance">
-            Fill in the form below to create your account
-          </p>
+          <p className="text-muted-foreground text-sm text-balance">Fill in the form below to create your account</p>
         </div>
         <Field>
           <FieldLabel htmlFor="name">Full Name</FieldLabel>
@@ -41,27 +31,20 @@ export function SignupForm({ onSubmit, loading, className, ...props }: any) {
         </Field>
         <Field>
           <FieldLabel htmlFor="email">Email</FieldLabel>
-          <Input
-            id="email"
-            type="email"
-            placeholder="m@example.com"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <Input id="email" type="email" placeholder="m@example.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
         </Field>
         <Field>
           <FieldLabel htmlFor="password">Password</FieldLabel>
           <Input
             id="password"
             type="password"
+            placeholder="********"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            minLength={8}
           />
-          <FieldDescription>
-            Must be at least 8 characters long.
-          </FieldDescription>
+          <FieldDescription className="text-xs">Must be at least 8 characters long.</FieldDescription>
         </Field>
         <Field>
           <Button type="submit" disabled={loading}>

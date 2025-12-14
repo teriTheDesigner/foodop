196e740fce3ab49caa286035b9ae6b26d2692f8e
teriTheDesigner <tere1109@stud.kea.dk>
2025-12-14 19:02:02 +0100

    Change toaster position

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.variable} antialiased`}>
         <ReduxProvider>{children}</ReduxProvider>
-        <Toaster />
+        <Toaster position="top-center" />
       </body>
     </html>
   );

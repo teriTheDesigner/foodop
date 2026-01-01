a3f9b39fa1505ed986d413ad36ad812ae2a29a38
teriTheDesigner <tere1109@stud.kea.dk>
2026-01-01 16:15:37 +0100

    Rewrite button text

--- app/dashboard/team/page.tsx
+++ app/dashboard/team/page.tsx
@@ -144,7 +144,7 @@ export default function TeamPage() {
         </div>
         <Button onClick={handleAddEmployee} className="gap-2">
           <Plus className="h-4 w-4" />
-          Add New Colleague
+          Add New
         </Button>
       </div>
 

e6d5b677b6a74ab3ae63cad795d9bee39e83c73f
teriTheDesigner <tere1109@stud.kea.dk>
2026-01-01 22:16:51 +0100

    Add getStatusColor to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getStatusColor = (status: string) => {
+  return status === "active" ? "bg-green-100 text-green-800" : status === "inactive" ? "bg-gray-100 text-gray-800" : "bg-red-100 text-red-800";
+};

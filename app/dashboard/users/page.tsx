3c3abada0a78e7b7eeb3fa1f6af06a03f5dc096c
teriTheDesigner <tere1109@stud.kea.dk>
2026-01-01 16:15:46 +0100

    Rewrite button text

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -195,7 +195,7 @@ export default function UsersPage() {
         </div>
         <Button onClick={handleAddUser} className="gap-2">
           <Plus className="h-4 w-4" />
-          Add User
+          Add New
         </Button>
       </div>
 
